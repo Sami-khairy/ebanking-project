9f577d96d1db53e8cb60ecf2ad880a7b850db35e
Sami-Khairy <samikhairy2003@gmail.com>
2025-05-08 20:45:37 +0100

    config DB & create entities

---
+++ ebanking-backend/src/main/java/ma/khairy/ebankingbackend/entities/AccountOperation.java
@@ -0,0 +1,23 @@
+package ma.khairy.ebankingbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ma.khairy.ebankingbackend.enums.OperationType;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class AccountOperation {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    private OperationType type;
+    @ManyToOne
+    private BankAccount bankAccount;
+}
